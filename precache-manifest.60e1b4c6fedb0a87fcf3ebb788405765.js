c687d54d0e15899c29e02f9d0e9f54a129f234d8
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-12-06 10:26:46 -0700

    Updates

---
+++ precache-manifest.60e1b4c6fedb0a87fcf3ebb788405765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "121d4f02586ace6ff1d9",
+    "url": "/React-Portfolio/static/js/main.121d4f02.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/React-Portfolio/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "a144e3d58a7a6bf0e58558b08b9cf728",
+    "url": "/React-Portfolio/static/media/headshot.a144e3d5.jpeg"
+  },
+  {
+    "revision": "263cf3aa5aed44e031e4a2f8c6517529",
+    "url": "/React-Portfolio/index.html"
+  }
+];
